3ecc6d9f4984ffc78ebd3b2ecb0a057a2c271ed2
masato nakatsuji <nakatsuji.sak19@gmail.com>
2023-12-09 17:53:04 +0900

    12/9

--- front/default/src/app/View/Page2View.ts
+++ front/default/src/app/View/Page2View.ts
@@ -1,10 +1,38 @@
 import View from "app/View/View";
+import { VDom, Response } from "Core";
 
 export default class PageView extends View{
 
-    public handle(){}
+    public ind : number = 0;
+    public target = VDom();
+
+    public handle(){
+        this.target = VDom("target");
+    }
 
     public clickHandle(){
         alert("click handle ..... ok");        
+    }
+
+    public clickHandle2(){
+        console.log("click Handle2 .... ok (view)");
+    }
+
+    public addHandle(){
+        this.ind++;
+
+        const item = Response.viewPart("page2item");
+
+        this.target.stamp(item, (i_) => {
+            i_.virtual("ind", this.ind);
+            i_.childVirtual("name").text = "name text " + this.ind;
+            i_.childVirtual("code").text = ("0000" + this.ind).slice(-5);
+            i_.childVirtual("deleteBtn").virtual("ind", this.ind);
+        });
+    }
+
+    public deleteHandle(i_){
+        const targetInd = i_.virtual("ind");
+        this.target.childDom().findOnVirtual("ind", targetInd).remove();
     }
 }
---
+++ front/default/src/app/ViewPart/Page2viewpartViewPart.ts
@@ -0,0 +1,8 @@
+import ViewPart from "ViewPart";
+
+export default class Page2viewpartViewPart extends ViewPart{
+
+    public clickHandle2(){
+        console.log("click Handle2 .... ok (viewpart)");
+    }
+}
